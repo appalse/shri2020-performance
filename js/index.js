7b8366a14b98eec8d734371e734522376f6c2115
appalse <frontend-hh@yandex.ru>
2020-04-07 17:59:23 +0300

    delete multimedia call

--- js/index.js
+++ js/index.js
@@ -5,6 +5,4 @@ window.addEventListener('DOMContentLoaded', () => {
 
     buttonsContainer.innerHTML = '<button class="button button_yellow" type="button">Да</button>' +
         '<button class="button">Нет</button>';
-    
-    document.querySelector('.multimedia').appendChild(multimediaScript);
 });
