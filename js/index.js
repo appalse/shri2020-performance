40f5be56a3e933eb0a4bb690779ceb21c356a801
appalse <frontend-hh@yandex.ru>
2020-04-12 15:56:51 +0300

    return index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
-window.addEventListener('DOMContentLoaded', function() {
-    var buttonsContainer = document.querySelector('.buttons-wrap');
+window.addEventListener('DOMContentLoaded', () => {
+    const buttonsContainer = document.querySelector('.buttons-wrap');
 
     buttonsContainer.innerHTML = '<button class="button button_yellow" type="button">Да</button>' +
         '<button class="button">Нет</button>';
